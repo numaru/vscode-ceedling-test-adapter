78411772eafd5685524fe9c350fa79dccbb29209
Kin Numaru <kin.numaru@protonmail.com>
2018-10-07 16:02:04 +0200

    :bug: Fix line of tests after a load
    The test regex is capturing some line feeds. They need to be added to the line count.

--- src/adapter.ts
+++ src/adapter.ts
@@ -295,7 +295,8 @@ export class CeedlingAdapter implements TestAdapter {
             let match = testRegex.exec(fileText);
             while (match != null) {
                 const testName = match[2];
-                const line = fileText.substr(0, match.index).split('\n').length;
+                let line = fileText.substr(0, match.index).split('\n').length - 1;
+                line = line + match[0].substr(0, match[0].search(/\S/g)).split('\n').length - 1;
                 currentTestSuitInfo.children.push({
                     type: 'test',
                     id: file + '::' + testName,
