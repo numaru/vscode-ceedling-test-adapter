53e46eaf6f2d6047dd346705099bbc0646424588
Kin Numaru <kin.numaru@protonmail.com>
2021-01-11 18:57:32 +0100

    :children_crossing: Add an info box when the user press F5 to debug

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,11 @@ function debugTestExecutable(): string | null {
     if (currentAdapter == null) return null;
     const currentExec = currentAdapter.getDebugTestExecutable();
     if (!currentExec) {
-        vscode.window.showErrorMessage("Not currently debugging a Ceedling Test");
+        vscode.window.showErrorMessage("No debug test executable found");
+        vscode.window.showInformationMessage(
+            "A debug configuration with a path containing `${command:ceedlingExplorer.debugTestExecutable}` " +
+            "cannot be started from F5 or the Run pannel. It should be started from a bug icon in the Test pannel."
+        );
         return null;
     }
     return currentExec;
